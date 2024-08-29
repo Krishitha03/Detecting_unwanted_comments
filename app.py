d8886f92f49ac89beaf548252bec48ce3db5f90c
Krishitha M <122097553+Krishitha03@users.noreply.github.com>
2024-08-29 19:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,5 +214,6 @@ def download_toxic_comments():
         download_name='toxic_comments.csv'
     )
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    port=int(os.environ.get("PORT",4000))
+    app.run(host='0.0.0.0',port=port)
 
